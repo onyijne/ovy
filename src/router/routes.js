acfa4b358755f0f4903dcd5229c1a1fca0e1c782
Samuel Onyijne <samuelord1@gmail.com>
2018-12-05 22:37:33 +0100

    fixed routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,9 @@
 import VueRouter from 'vue-router'
 
-let LandingPage = require('./pages/LandingPage.vue');
-let AboutPage = require('./pages/AboutPage.vue');
-let LoginPage = require('./pages/LoginPage.vue');
+let AboutPage = require('../pages/AboutPage.vue');
+let LoginPage = require('../pages/LoginPage.vue');
 
 let routes = [
-    { path: '/', component: LandingPage, name: 'landing'},
     { path: '/about', component: AboutPage, name: 'about'},
     { path: '/login', component: LoginPage, name: 'login'}
 ];
